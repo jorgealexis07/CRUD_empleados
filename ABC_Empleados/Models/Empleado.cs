cf59294bdf038ef1f2bda8713d685b971264b605
jorgealexis07 <201711104@tese.edu.mx>
2023-04-26 08:14:42 -0600

    Agrego sus respectivas validaciones

--- ABC_Empleados/Models/Empleado.cs
+++ ABC_Empleados/Models/Empleado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,9 +9,20 @@ namespace ABC_Empleados.Models
     public partial class Empleado
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Apellido Paterno es requerido")]
+        [Display(Name = "Apellido Paterno")]
         public string ApellidoPaterno { get; set; }
+
+        [Required(ErrorMessage = "El Apellido Materno es requerido")]
+        [Display(Name = "Apellido Materno")]
         public string ApellidoMaterno { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es requerida")]
+        [Display(Name = "Fecha Nacimiento")]
         public DateTime? FechaNacimiento { get; set; }
         public int? EstatusId { get; set; }
 
